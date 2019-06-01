033185fab8504cd6d45b173895cce3f46e1262ce
zanton173 <zanton173@gmail.com>
2019-06-01 00:47:46 -0400

    Commit #7 Found connection to database and wrote first query

--- Sections/B/B8.php
+++ Sections/B/B8.php
@@ -1,4 +1,17 @@
-<?php include 'Queries.php';?>
+<?php include 'Queries.php'; 
+
+$DoThings = new Queries();
+if(isset($_POST['submit'])){
+    
+    $partNum = $_POST['partNum'];
+    $linesUsed = $_POST['lines'];
+    $qty = $_POST['quantity'];
+    
+    $DoThings->insertNewPart($partNum, $linesUsed, $qty);
+}
+
+
+?>
 <html>
 <body class="bodyBackground">
 <head>
@@ -8,13 +21,17 @@
 	Application</h1>
 
 <div style="height: 550px;">
+<h4 style='text-align: center;'>Putaway Item</h4>
+<form action='B8.php' method="post">
+	<input type='text' name='partNum' placeholder='Part Number'>
+	<input type='text' name='lines' placeholder='Lines Used In'>
+	<input type='text' name='quantity' placeholder='Quantity'>
+	<input type='submit' name='submit' placeholder='submit'>
+</form>
 	
-	<ol>
-	<li>item 2</li>
-	<li>item 3</li>
-	<li>item 4</li>
-	</ol>
+	
 </div>
+
 <div class="centering">
 	<button style="height: 50px;"
 		onclick="window.location.href = '../../Home.html';">Home
--- Sections/B/Queries.php
+++ Sections/B/Queries.php
@@ -1,5 +1,28 @@
 <?php
+class Queries{
 
 
+function insertNewPart($partNumber, $linesUsed, $qty){
+    $hostname = "localhost";
+    $dbusername = "root";
+    $dbpass = "";
+    $dbname = "hine";
+
+    $connect = mysqli_connect($hostname, $dbusername, $dbpass, $dbname);
+    if (!$connect) {
+        die("connection failed");
+    } else {echo "function in use";}
+    $query = "INSERT INTO parts(partNumber, linesUsed, qty) VALUES ('$partNumber', '$linesUsed', '$qty')";
+    //$query = "SELECT * FROM parts";
+    $result = mysqli_query($connect, $query);
+    
+    if (!$result) {
+        echo "query failed";
+    } else {
+        echo "Query worked";
+    }
+}
+
+}
 
 ?>
